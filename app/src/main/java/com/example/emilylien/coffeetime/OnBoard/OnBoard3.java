4c7049fb8ee890506f7136f4cf3904ca040792a6
mlelien <emily.c.lien@gmail.com>
2018-05-10 15:37:32 +0200

    added butterknife, more work on onboarding skeleton

---
+++ app/src/main/java/com/example/emilylien/coffeetime/OnBoard/OnBoard3.java
@@ -0,0 +1,13 @@
+package com.example.emilylien.coffeetime.OnBoard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnBoard3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board3);
+    }
+}
