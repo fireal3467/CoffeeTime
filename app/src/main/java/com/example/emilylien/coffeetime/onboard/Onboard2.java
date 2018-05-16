960845d020d2b21c68e394c4ff296a8b040e4e27
mlelien <emily.c.lien@gmail.com>
2018-05-16 04:43:09 +0200

    awful

--- app/src/main/java/com/example/emilylien/coffeetime/onboard/Onboard2.java
+++ app/src/main/java/com/example/emilylien/coffeetime/onboard/Onboard2.java
@@ -1,13 +1,13 @@
 package com.example.emilylien.coffeetime.onboard;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.emilylien.coffeetime.R;
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -20,37 +20,41 @@ import butterknife.OnClick;
 public class Onboard2 extends AppCompatActivity {
     @BindView(R.id.btnOnboard2Next) Button btnOnboard2Next;
     @BindView(R.id.btnOnboard2Back) Button btnOnboard2Back;
-    @BindView(R.id.tilAge) MaterialEditText tilAge;
+    @BindView(R.id.etAge) MaterialEditText etAge;
+    @BindView(R.id.etMaximum) MaterialEditText etMaximum;
+    @BindView(R.id.etMinimum) MaterialEditText etMinimum;
+    @BindView(R.id.switchPregnant) Switch switchPregnant;
+    @BindView(R.id.tvRecommendedDailyLimitAnswer) TextView tvRecommendedDailyLimitAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board2);
         ButterKnife.bind(this);
-
-        tilAge.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                String regex = "^[1-9]\\d*$";
-//                String input = s.toString();
-//                if (!input.matches(regex)) {
-//                    tilAge.setError("Invalid symbol");
-//                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
     }
 
     @OnClick(R.id.btnOnboard2Next) void nextClicked() {
+        int max;
+        try {
+            max = Integer.parseInt(etMaximum.getText().toString());
+        } catch (Exception e) {
+            max = 400;
+        }
+        SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.USER_SETTINGS), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(getString(R.string.MAX), max);
+        editor.commit();
+
+        int min;
+        try {
+            min = Integer.parseInt(etMinimum.getText().toString());
+        } catch (Exception e) {
+            min = 100;
+        }
+        editor = sharedPreferences.edit();
+        editor.putInt(getString(R.string.MIN), min);
+        editor.commit();
+
         Intent intent = new Intent(Onboard2.this, Onboard3.class);
         startActivity(intent);
     }
@@ -58,10 +62,5 @@ public class Onboard2 extends AppCompatActivity {
     @OnClick(R.id.btnOnboard2Back) void backClicked() {
         Intent intent = new Intent(Onboard2.this, Onboard1.class);
         startActivity(intent);
-    }
-
-    @OnClick(R.id.rlLayout) void hideSoftKeyboard() {
-        InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
     }
 }
